080bfa72c51985cdbcba3553b51c32a55e26b732
AnnaMintel <annikamintel99@yandex.ru>
2022-08-22 18:39:50 +0300

    feat: wrote reducer and added new tests

--- src/store/tasks.test.ts
+++ src/store/tasks.test.ts
@@ -1,5 +1,4 @@
-import { sum } from "./tasks"
-
+import { ActionType, calculator, div, mult, sub, sum } from "./tasks"
 
  test ("check sum of numbers", () => {
     // 1 - тестовые данные
@@ -10,3 +9,36 @@ import { sum } from "./tasks"
     // 3 - проверка результата
     expect(result).toBe(10)
  })
+
+ //2 way sun function
+ test ("check sum of numbers", () => {
+    expect(sum(3,7)).toBe(10)
+ }) 
+
+ test ("check * of numbers", () => {
+    expect(mult(3,7)).toBe(21)
+ })
+
+ test ("check divide of numbers", () => {
+    expect(div(35,7)).toBe(5)
+ })
+
+ test ("check minus of numbers", () => {
+    expect(sub(10,7)).toBe(3)
+ })
+
+
+test("calculator", () => {
+    const actionSum: ActionType = {type: "sum", number: 7}
+    expect(calculator(3,  actionSum)).toBe(10)
+
+    const actionMult: ActionType = {type: "mult", number: 7}
+    expect(calculator(3, actionMult) ).toBe(21)
+
+    const actionDiv: ActionType = {type: "div", number: 7}
+    expect(calculator(35, actionDiv) ).toBe(5)
+
+    const actionSub: ActionType = {type: "sub", number: 7}
+    expect(calculator(10, actionSub) ).toBe(3)
+})
+  
--- src/store/tasks.ts
+++ src/store/tasks.ts
@@ -1,3 +1,33 @@
 export const sum = (a: number, b: number) => {
-    return a+b;
+    return a + b;
+}
+export const mult = (a: number, b: number): number => {
+    return a * b;
+}
+export const div = (a: number, b: number): number => {
+    return a / b;
+}
+export const sub = (a: number, b: number): number => {
+    return a - b;
+}
+
+// a - data (исходный параметр), {type: "sum" (тип измененияЖ  плюс, минус и тд), number(второе число) }
+
+export type ActionType = {
+    type: "sum" | "mult" | "div" | "sub"
+    number: number
+}
+export const calculator = (state: number, action: ActionType) => {
+    switch (action.type) {
+        case "sum":
+            return state + action.number
+        case "mult":
+            return state * action.number
+        case "div":
+            return state / action.number
+        case "sub":
+            return state - action.number
+        default: 
+            return state
+    }
 }
